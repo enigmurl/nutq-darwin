8ed72a7abccd06079e65d4d72c2b08d478141d5b
enigmurl <43832426+enigmurl@users.noreply.github.com>
2023-05-28 11:46:55 -0700

    Initial Commit

---
+++ nutqdarwin/nutqdarwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutqdarwinApp.swift
+//  nutqdarwin
+//
+//  Created by Manu Bhat on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct nutqdarwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
